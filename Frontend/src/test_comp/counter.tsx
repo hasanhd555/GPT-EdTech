0174b6298b69ada056568042d582e9aea1c24e4e
hasanhd555 <hasanhd555@gmail.com>
2024-02-27 01:10:35 +0500

    Basic redux setup

---
+++ Frontend/src/test_comp/counter.tsx
@@ -0,0 +1,15 @@
+
+
+const Counter:React.FC =()=> {
+    const count =0 ;
+    return (
+      <div>
+        COUNT:{count}
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
+  
+  export default Counter;
+  
