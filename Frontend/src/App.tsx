0ad528f73c5d1502a9ac23f02bcd4366886e670b
Muhammad Musa Zulfiqar <musazulfqar4@gmail.com>
2024-02-28 00:34:20 +0500

    Navbar Done

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { Routes,Route } from 'react-router-dom';
-import Counter from './test_comp/counter';
-import Unpersisted_Counter from './test_comp/unpersisted_ctr';
+import { Routes, Route } from "react-router-dom";
+import Counter from "./test_comp/counter";
+import Unpersisted_Counter from "./test_comp/unpersisted_ctr";
+import Navbar from "./components/Navbar and Footer/Navbar";
 
 function App() {
   return (
     <Routes>
-      <Route path="/ctr_test" element={<Counter/>}/>
-      <Route path ="/unpersisted_ctr_test" element={<Unpersisted_Counter/>}/>
+      <Route path="/ctr_test" element={<Counter />} />
+      <Route path="/unpersisted_ctr_test" element={<Unpersisted_Counter />} />
+      <Route path="/nav" element={<Navbar />} />
     </Routes>
-
   );
 }
 
---
+++ Frontend/src/components/Navbar and Footer/Navbar.tsx
@@ -0,0 +1,63 @@
+import {
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Container,
+  Button,
+  InputGroup,
+} from "react-bootstrap";
+import Styles from "./Navbar.module.css";
+
+function NavbarComp() {
+  return (
+    <Navbar expand="lg" className="bg-body-white shadow">
+      <Container fluid className="px-5">
+        <Navbar.Brand>
+          <img
+            src="/IntelliLearn Logo.svg"
+            width="40"
+            height="50"
+            className="d-inline-block align-top"
+            alt="IntelliLearn logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Explore</Nav.Link>
+            <Nav.Link href="#link">About Us</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="#link">Summarizer</Nav.Link>
+
+            <Form className="d-flex px-2 border-secondary">
+              <InputGroup>
+                <Button
+                  variant="outline-secondary"
+                  className={` ${Styles.customhighlight}`}
+                >
+                  <span className="bi bi-search "></span>
+                </Button>
+                <FormControl
+                  type="search"
+                  placeholder="Search a Course"
+                  className={`border-secondary ${Styles.customhighlight}`}
+                  aria-label="Search"
+                />
+              </InputGroup>
+            </Form>
+          </Nav>
+          <Nav>
+            <Button className="mx-2" variant="primary">
+              Login
+            </Button>
+            <Button variant="outline-primary">Sign Up</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;
