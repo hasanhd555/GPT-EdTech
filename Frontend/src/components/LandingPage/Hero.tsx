e984fb26b004ee3833e7c4051c804c29be167ece
hasanhd555 <hasanhd555@gmail.com>
2024-03-21 00:28:30 +0500

    Chnaged button and background colours

--- Frontend/src/components/LandingPage/Hero.tsx
+++ Frontend/src/components/LandingPage/Hero.tsx
@@ -6,9 +6,9 @@ const Hero: React.FC = () => {
     <Container fluid className={`${styles["custom-purple-blue-gradient"]} h-100`}> {/* Set background color inline */}
       <Row className="h-100">
         <Col xs={6} className="d-flex flex-column justify-content-between px-md-5 py-5"> {/* Left section */}
-          <p className="text-semi-bold fs-6" style={{ fontFamily: 'Saira' }}> Successful coaches are visionaries </p>
-          <h1 className="display-3 mb-4" style={{ lineHeight: '1.125' }}> Good coaching is good teaching & <br /> nothing else. </h1>
-          <Button variant="primary" size="sm" className="px-md-5 align-self-start custom-small-btn"> View Courses </Button>
+          <p className="text-semi-bold fs-6 text-white" style={{ fontFamily: 'Saira' }}> SUCCESSFUL COACHES ARE VISIONARIES </p>
+          <h1 className="display-3 mb-4 text-white" style={{ lineHeight: '1.125' }}> Good coaching is good teaching & <br /> nothing else. </h1>
+          <Button size="sm" className="px-md-5 align-self-start custom-small-btn btn-outline-light bg-transparent text-light"> View Courses </Button>
         </Col>
         <Col xs={6}> {/* Right section (can be empty or contain other content) */}
         </Col>
