4af10410d9bd8c95e2e00d745078dc4ffe9270d5
hasanhd555 <hasanhd555@gmail.com>
2024-04-01 00:36:50 +0500

    Completed Competition Pitch

--- Frontend/src/components/LandingPage/CompetitionPitch/CompetitonPitch.tsx
+++ Frontend/src/components/LandingPage/CompetitionPitch/CompetitonPitch.tsx
@@ -10,6 +10,47 @@ import {
   InputGroup,
 } from "react-bootstrap";
 import styles from "./CompetitionPitch.module.css";
+import { img1, img2, img3, img4, img5, img6 } from "./CompUrls";
+
+interface carddata{
+  url: string;
+  heading: string;
+  text: string;
+}
+
+let cardData: carddata[] = [
+  {
+    url: img1,
+    heading: "Digital Platform",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  },
+  {
+    url: img2,
+    heading: "Optimal Ideation",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  },
+  {
+    url: img3,
+    heading: "Favorable Setting",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  },
+  {
+    url: img4,
+    heading: "Effective Interaction",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  },
+  {
+    url: img5,
+    heading: "Flexible Time",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  },
+  {
+    url: img6,
+    heading: "Reliable",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis."
+  }
+
+]
 
 const CompetitionPitch: React.FC = () => {
   return (
@@ -22,14 +63,15 @@ const CompetitionPitch: React.FC = () => {
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.....
       </p>
       <Row className="justify-content-center py-5">
-        {[1, 2, 3, 4, 5, 6].map((index) => (
-          <Col key={index} xs={12} md={4} className="mb-4">
+        {cardData.map((index) => (
+          <Col  xs={12} md={4} className="mb-4">
             <Card className={`${styles["hover-effect"]} text-center`}>
               <Card.Body>
-                <img src={`logo${index}.png`} alt={`Logo ${index}`} className="mb-3" />
-                <Card.Title>Heading {index}</Card.Title>
+                <img src={index["url"]} alt={``} className="mb-3" />
+                {/* Assuming img1, img2, etc. are the correct variables containing image URLs */}
+                <Card.Title>{index["heading"]} </Card.Title>
                 <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse cursus malesuada facilisis.
+                  {index["text"]}
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -41,4 +83,5 @@ const CompetitionPitch: React.FC = () => {
 };
 
 export default CompetitionPitch;
+
 
--- Frontend/src/components/LandingPage/Testimonials/Testimonials.tsx
+++ Frontend/src/components/LandingPage/Testimonials/Testimonials.tsx
@@ -1,26 +1,85 @@
+import React from "react";
+import { Card, Container } from "react-bootstrap";
+import styles from "./Testimonials.module.css";
 
-import {
-    Card,
-    Container,
-    Row,
-    Col,
-    Button,
-    Form,
-    FormControl,
-    InputGroup,
-  } from "react-bootstrap";
-  import styles from "./Testimonials.module.css";
+const Testimonials: React.FC = () => {
+  return (
+    <Container fluid className={`${styles["background"]} text-center`}>
+      <h2 className={`${styles["green-text"]} py-5`}>What our Students Say About Us</h2>
+      <div className="d-flex justify-content-around">
+        <Card className="rounded">
+          <Card.Body>
+            <div className="text-left">
+              <img
+                src="image_url_1"
+                alt="Student 1"
+                className="img-fluid rounded-circle"
+                style={{ width: "80px" }}
+              />
+              <Card.Title>Name 1</Card.Title>
+              <Card.Text>
+                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Card.Text>
+              <img
+                src="bottom_left_image_url_1"
+                alt="Bottom Left Image 1"
+                className="img-fluid"
+                style={{ width: "40px" }}
+              />
+            </div>
+          </Card.Body>
+        </Card>
 
-const Testimonials:React.FC = () => {
-
-    return (
-        <Container fluid className={`${styles["background"]} text-center`}>
-            <h2 className={`${styles["green-text"]} py-5`}>
-            What our Students <br/>Say About Us
-            </h2>
-        </Container>
-
-    );
+        {/* Repeat the same structure for the remaining two cards */}
+        {/* Card 2 */}
+        <Card className="rounded">
+          <Card.Body>
+            <div className="text-left">
+              <img
+                src="image_url_1"
+                alt="Student 1"
+                className="img-fluid rounded-circle"
+                style={{ width: "80px" }}
+              />
+              <Card.Title>Name 1</Card.Title>
+              <Card.Text>
+                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Card.Text>
+              <img
+                src="bottom_left_image_url_1"
+                alt="Bottom Left Image 1"
+                className="img-fluid"
+                style={{ width: "40px" }}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+        {/* Card 3 */}
+        <Card className="rounded">
+          <Card.Body>
+            <div className="text-left">
+              <img
+                src="image_url_1"
+                alt="Student 1"
+                className="img-fluid rounded-circle"
+                style={{ width: "80px" }}
+              />
+              <Card.Title>Name 1</Card.Title>
+              <Card.Text>
+                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </Card.Text>
+              <img
+                src="bottom_left_image_url_1"
+                alt="Bottom Left Image 1"
+                className="img-fluid"
+                style={{ width: "40px" }}
+              />
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+    </Container>
+  );
 };
 
 export default Testimonials;
