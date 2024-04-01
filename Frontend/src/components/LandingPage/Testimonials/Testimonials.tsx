a4b3ed2054b2896affa5e9cbb0134e2dd13e9fbd
hasanhd555 <hasanhd555@gmail.com>
2024-04-01 16:58:19 +0500

    implemented testimonial

--- Frontend/src/components/LandingPage/Testimonials/Testimonials.tsx
+++ Frontend/src/components/LandingPage/Testimonials/Testimonials.tsx
@@ -1,83 +1,87 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import styles from "./Testimonials.module.css";
-
+import { img1, img2, img3, stars } from "./TestimonialUrls";
 const Testimonials: React.FC = () => {
   return (
-    <Container fluid className={`${styles["background"]} text-center`}>
+    <Container fluid className={`${styles["background"]} text-center py-3 px-5`}>
       <h2 className={`${styles["green-text"]} py-5`}>What our Students Say About Us</h2>
-      <div className="d-flex justify-content-around">
-        <Card className="rounded">
-          <Card.Body>
-            <div className="text-left">
-              <img
-                src="image_url_1"
-                alt="Student 1"
-                className="img-fluid rounded-circle"
-                style={{ width: "80px" }}
-              />
-              <Card.Title>Name 1</Card.Title>
-              <Card.Text>
-                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </Card.Text>
-              <img
-                src="bottom_left_image_url_1"
-                alt="Bottom Left Image 1"
-                className="img-fluid"
-                style={{ width: "40px" }}
-              />
-            </div>
-          </Card.Body>
-        </Card>
+      <Row className="justify-content-around">
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <div className="text-left">
+                <img
+                  src={img1}
+                  alt="Student 1"
+                  className="img-fluid rounded-circle"
+                  style={{ width: "80px" }}
+                />
+                <Card.Title>Ronald Richards</Card.Title>
+                <Card.Text>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                </Card.Text>
+                <img
+                  src={stars}
+                  alt="Bottom Left Image 1"
+                  className="img-fluid"
+                  style={{ width: "40px" }}
+                />
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
 
-        {/* Repeat the same structure for the remaining two cards */}
-        {/* Card 2 */}
-        <Card className="rounded">
-          <Card.Body>
-            <div className="text-left">
-              <img
-                src="image_url_1"
-                alt="Student 1"
-                className="img-fluid rounded-circle"
-                style={{ width: "80px" }}
-              />
-              <Card.Title>Name 1</Card.Title>
-              <Card.Text>
-                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </Card.Text>
-              <img
-                src="bottom_left_image_url_1"
-                alt="Bottom Left Image 1"
-                className="img-fluid"
-                style={{ width: "40px" }}
-              />
-            </div>
-          </Card.Body>
-        </Card>
-        {/* Card 3 */}
-        <Card className="rounded">
-          <Card.Body>
-            <div className="text-left">
-              <img
-                src="image_url_1"
-                alt="Student 1"
-                className="img-fluid rounded-circle"
-                style={{ width: "80px" }}
-              />
-              <Card.Title>Name 1</Card.Title>
-              <Card.Text>
-                Text of testimonial goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </Card.Text>
-              <img
-                src="bottom_left_image_url_1"
-                alt="Bottom Left Image 1"
-                className="img-fluid"
-                style={{ width: "40px" }}
-              />
-            </div>
-          </Card.Body>
-        </Card>
-      </div>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <div className="text-left">
+                <img
+                  src={img2}
+                  alt="Student 2"
+                  className="img-fluid rounded-circle"
+                  style={{ width: "80px" }}
+                />
+                <Card.Title>Wade Warren</Card.Title>
+                <Card.Text>
+                Cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Reprehenderit in voluptate velit esse 
+                </Card.Text>
+                <img
+                  src={stars}
+                  alt="Bottom Left Image 2"
+                  className="img-fluid"
+                  style={{ width: "40px" }}
+                />
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <div className="text-left">
+                <img
+                  src={img3}
+                  alt="Student 3"
+                  className="img-fluid rounded-circle"
+                  style={{ width: "80px" }}
+                />
+                <Card.Title>Jacob Jones</Card.Title>
+                <Card.Text>
+                Esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Duis aute irure dolor in reprehenderit in voluptate velit 
+                </Card.Text>
+                <img
+                  src={stars}
+                  alt="Bottom Left Image 3"
+                  className="img-fluid"
+                  style={{ width: "40px" }}
+                />
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
