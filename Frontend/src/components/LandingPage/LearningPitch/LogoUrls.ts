de051de273e43494270fb467cde5a0934eb4f09f
hasanhd555 <hasanhd555@gmail.com>
2024-04-05 18:18:50 +0500

    finished Learning pitch

--- Frontend/src/components/LandingPage/LearningPitch/LogoUrls.ts
+++ Frontend/src/components/LandingPage/LearningPitch/LogoUrls.ts
@@ -1,5 +1,5 @@
-const logo2="https://res.cloudinary.com/ddvjtkdge/image/upload/v1711828330/sakvlkvix56jvz4vh8mj.png";
-const logo1="https://res.cloudinary.com/ddvjtkdge/image/upload/v1711828330/zdtcfmmuar1rbuechdhp.png";
-const logo3="https://res.cloudinary.com/ddvjtkdge/image/upload/v1711828330/ax1fej9ghmpodz935clp.png";
+const logo1="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712322544/Learn_czpbzt.svg";
+const logo2="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712322543/Graduate_ti5gpg.svg";
+const logo3="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712322544/Work_pa3d8k.svg";
 
 export { logo1, logo2, logo3 };
