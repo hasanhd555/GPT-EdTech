2d7a1de1ad0bc2f166610a3f02cf20a136a0246a
hasanhd555 <hasanhd555@gmail.com>
2024-04-11 12:57:23 +0500

    Hero changes

---
+++ Frontend/src/components/LandingPage/HeroSection/HeroUrls.ts
@@ -0,0 +1,4 @@
+const Hero_Pic="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712818883/young-smiling-confident-woman-using-laptop-computer-looking-camera-isolated-white-background-removebg-preview_1_i32ntb.svg"
+const Hero_testimonial="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712821649/Hero_test_crvwqa.svg"
+const Green_stars="https://res.cloudinary.com/ddvjtkdge/image/upload/v1712821649/Green_Stars_s9fkwc.svg"
+export {Hero_Pic,Hero_testimonial,Green_stars}
