f88958c490ffbaf7f3aba0086e372bb9a84240f6
Fayzan Ali Akhtar <fayzan585@gmail.com>
2024-04-12 14:30:24 +0500

    Admin code refactoring: LessonsEditComponent.tsx added!

--- Frontend/src/components/Courses/EditCourse.tsx
+++ Frontend/src/components/Courses/EditCourse.tsx
@@ -4,50 +4,34 @@ import Card from "react-bootstrap/Card";
 import { Spinner, Button } from "react-bootstrap";
 import {
   getCourseAllInfoAPI,
-  updateLessonAPI,
   updateQuestionAPI,
 } from "../../constant";
-import {  lesson_type, question_type } from "../../constant";
+import {  question_type } from "../../constant";
 import {
   Formik,
   Form,
   Field,
   ErrorMessage,
   FieldArray,
-  FormikHelpers,
 } from "formik";
 import * as Yup from "yup";
 import { useAppSelector } from "../../redux/hooks"; // Redux hook to access the store
 import { NavigateFunction, useNavigate } from "react-router"; // Hook for navigation
 import EditCourseDetails from "./EditCourseDetails"; // Component for editing course details
 import EditCourseImage from "./EditCourseImage";
+import LessonsEditComponent from "./LessonsEditComponent";
 
 function EditCourse() {
   const [courseId, setCourseId] = useState("");
-  const [lessons, setLessons] = useState<lesson_type[]>([]);
   const [questions, setQuestions] = useState<question_type[]>([]);
-  const [editModeLessons, setEditModeLessons] = useState(false);
-  const [editableLessons, setEditableLessons] = useState<lesson_type[]>([]);
   const [editModeQuestions, setEditModeQuestions] = useState(false);
   const [editableQuestions, setEditableQuestions] = useState<question_type[]>(
     []
   );
 
-  const [savingLessons, setSavingLessons] = useState(false);
   const [savingQuestions, setSavingQuestions] = useState(false);
   const navigate: NavigateFunction = useNavigate();
   const { isAdmin, email, _id } = useAppSelector((state) => state.User);
-
-  // Validation schema for a single lesson
-  const lessonSchema = Yup.object().shape({
-    title: Yup.string().required("Title is required"),
-    content: Yup.string().required("Content is required"),
-  });
-
-  // Validation schema for the array of lessons
-  const validationSchema = Yup.object().shape({
-    lessons: Yup.array().of(lessonSchema),
-  });
 
   // Validation schema for a single question
   const questionSchema = Yup.object().shape({
@@ -83,9 +67,7 @@ function EditCourse() {
           );
           const { course, lessons, questions, imageUrl } = response.data;
 
-          setLessons(lessons);
           setQuestions(questions);
-          setEditableLessons(lessons);
           setEditableQuestions([...questions]);
         } catch (error) {
           console.error("Error fetching course info", error);
@@ -134,41 +116,7 @@ function EditCourse() {
     setEditModeQuestions(false);
   };
 
-  const handleSaveLessonsChanges = async (
-    lessons: lesson_type[],
-    actions: FormikHelpers<{ lessons: lesson_type[] }>
-  ) => {
-    try {
-      setSavingLessons(true); // Set loading to true before the request
-      const params = new URLSearchParams(window.location.search);
-      const courseId = params.get("id");
-      // Assuming you're updating all lessons at once or individually in a loop
-      for (const lesson of lessons) {
-        await axios.put(`${updateLessonAPI}/${lesson._id}`, lesson);
-      }
-      // After all lessons are updated, you might want to refetch course info or directly update the state
-      actions.setSubmitting(false);
-      // After all lessons are updated, refetch course info
-      const response = await axios.get(
-        `${getCourseAllInfoAPI}?courseId=${courseId}`
-      );
-      const { lessons: updatedLessons } = response.data;
 
-      setLessons(updatedLessons); // Update lessons with the newly fetched data
-      setEditableLessons(updatedLessons); // Update editable lessons with the latest data
-      setEditModeLessons(false); // Exit edit mode for lessons
-    } catch (error) {
-      console.error("Error updating lessons", error);
-      actions.setSubmitting(false);
-    } finally {
-      setSavingLessons(false); // Set loading to false after the request
-    }
-  };
-
-  // Lessons Edit Mode Toggle
-  const toggleEditModeLessons = () => {
-    setEditModeLessons(!editModeLessons);
-  };
 
   return (
     <div className="mt-2 mb-3">
@@ -183,121 +131,7 @@ function EditCourse() {
 
         <EditCourseImage courseId={courseId} />
 
-        {/* Lessons Editing Interface */}
-        <div className="mx-auto border rounded border-primary p-3 m-3">
-          <h3 className="display-4 text-center fw-bold mt-4 text-primary">
-            Lessons Section
-          </h3>
-          {editModeLessons ? (
-            <Formik
-              initialValues={{ lessons: editableLessons }}
-              validationSchema={validationSchema}
-              onSubmit={(values, actions) => {
-                handleSaveLessonsChanges(values.lessons, actions);
-              }}
-            >
-              {({ values, handleSubmit, isSubmitting }) => (
-                <Form onSubmit={handleSubmit}>
-                  <FieldArray name="lessons">
-                    {({ insert, remove, push }) => (
-                      <div>
-                        {values.lessons.length > 0 &&
-                          values.lessons.map(
-                            (lesson: lesson_type, index: number) => (
-                              <div key={index} className="card mb-3">
-                                <div className="card-body">
-                                  <h3 className="card-title text-primary text-center fw-bold">
-                                    Lesson {index + 1}
-                                  </h3>
-                                  <h5 className="fw-bold">Title</h5>
-                                  <Field
-                                    name={`lessons.${index}.title`}
-                                    placeholder="Title"
-                                    type="text"
-                                    className="form-control mb-2  text-primary" // Add mb-2 for margin bottom to the title field
-                                    style={{ marginBottom: "10px" }} // Alternatively, you can use inline styles for specific margin values
-                                  />
-                                  <ErrorMessage
-                                    name={`lessons.${index}.title`}
-                                    component="div"
-                                    className="text-danger" // Apply red color style
-                                  />
-                                  <h5 className="fw-bold">Content</h5>
-                                  <Field
-                                    name={`lessons.${index}.content`}
-                                    placeholder="Content"
-                                    as="textarea"
-                                    className="form-control mb-2  text-primary" // Add mb-2 for margin bottom to the content field
-                                    style={{
-                                      marginBottom: "10px",
-                                      marginTop: "10px",
-                                    }} // Adding margin top and bottom for spacing around the content field
-                                  />
-                                  <ErrorMessage
-                                    name={`lessons.${index}.content`}
-                                    component="div"
-                                    className="text-danger" // Apply red color style
-                                  />
-                                </div>
-                              </div>
-                            )
-                          )}
-                      </div>
-                    )}
-                  </FieldArray>
-                  <div className="d-flex justify-content-center mt-3">
-                    <button
-                      type="submit"
-                      className="btn btn-success me-2"
-                      disabled={isSubmitting}
-                    >
-                      Save All Lessons
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-danger"
-                      onClick={toggleEditModeLessons}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          ) : (
-            <>
-              {/* Corrected Display mode for lessons */}
-              {lessons.map((lesson, index) => (
-                <Card key={lesson._id || index} className="mt-2 mb-2">
-                  <Card.Body>
-                    {/* className="card-title text-primary text-center fw-bold" */}
-                    <Card.Title className="text-primary text-center fw-bold fs-3">
-                      Lesson {index + 1}
-                    </Card.Title>
-                    <Card.Text>
-                      <span className="fw-bold">Title : </span>
-                      <span className="card-title text-primary text-center fw-bold">
-                        {lesson.title}
-                      </span>
-                    </Card.Text>
-                    <Card.Text>
-                      <span className="fw-bold">Content : </span>
-                      <span className="card-title text-primary text-center fw-bold">
-                        {lesson.content}
-                      </span>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              ))}
-              <div className="text-center mt-3 mb-3">
-                <Button variant="primary" onClick={toggleEditModeLessons}>
-                  Edit Lessons
-                </Button>
-              </div>
-            </>
-          )}
-          {savingLessons && <Spinner animation="border" variant="primary" />}
-        </div>
+        <LessonsEditComponent courseId={courseId} />
 
         {/* Quiz Questions Viewing and Editing Interface */}
         <div className="mx-auto border rounded border-primary p-3">
