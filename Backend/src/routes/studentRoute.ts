b30564e9175319163be3bb5441cc2a4bca9c46c5
Muhammad Zaeem Rizwan <zaeemr49@gmail.com>
2024-04-12 14:19:58 +0500

    Refactored-studentRoute

--- Backend/src/routes/studentRoute.ts
+++ Backend/src/routes/studentRoute.ts
@@ -1,21 +1,32 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-// Controllers 
-const {signup,login,getOneStudent,getAllStudents,updateStudent,uploadImage } = require('../controllers/studentController');
+// Controllers
+const {
+  signup,
+  login,
+  getOneStudent,
+  getAllStudents,
+  updateStudent,
+  uploadImage,
+} = require("../controllers/studentController");
 
-// SignUp Route 
-router.route('/signup').post(signup);
-// Log In Route 
-router.route('/login').post(login);
-// Get a Specific Student 
-router.route('/').get(getOneStudent);
+// SignUp Route
+router.route("/signup").post(signup);
+
+// Log In Route
+router.route("/login").post(login);
+
+// Get a Specific Student
+router.route("/").get(getOneStudent);
+
 // Get All Students
-router.route('/all').get(getAllStudents);
+router.route("/all").get(getAllStudents);
 
-router.put('/update', updateStudent);
+// Route to update a student's details
+router.put("/update", updateStudent);
 
-router.post('/upload-image', uploadImage);
+// Route to upload an image associated with a student
+router.post("/upload-image", uploadImage);
 
-
-
+// Exporting the router to be used in other parts of the application
 module.exports = router;
